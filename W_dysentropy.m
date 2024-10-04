57e7e2edd45e4efc03cbfa16882e4f3ceb8f4dbe
Rong Li <143572894+RongLiNeuroLab@users.noreply.github.com>
2024-10-05 00:48:48 +0800

    Update W_dysentropy.m

--- W_dysentropy.m
+++ W_dysentropy.m
@@ -1,11 +1,11 @@
-function [common,specific,enmat] = W_dysentropy(mat)
+function [stable,dynamic,enmat] = W_dysentropy(mat)
 %mat: timepoints*roi
 len = size(mat,1);
 cols = size(mat,2);
 enmat = zeros(1,cols);
 
-common = zeros(1,cols);
-specific = zeros(1,cols);
+stable = zeros(1,cols);
+dynamic = zeros(1,cols);
 
 for col = 1 :cols
     seires = mat(:,col);
@@ -18,51 +18,11 @@ for col = 1 :cols
     end
     enmat(1,col) = -entro;
 
-common(1,col) = (sum(seires == 2) + sum(seires == 3))/len;
-specific(1,col) = (sum(seires == 1) + sum(seires == 4))/len;
+stable(1,col) = (sum(seires == 2) + sum(seires == 3))/len;
+dynamic(1,col) = (sum(seires == 1) + sum(seires == 4))/len;
     
-    %s_c(1,col) = specific(1,col) / common(1,col);
-    
-  
-%     seires = mat(:,col);
-%     entro = 0;
-%     c = sum(seires == 2) + sum(seires == 3);
-%     for ind = 2:3
-%         p = sum(seires == ind)/c;
-%         if p ~= 0 
-%         entro = p*log2(p) + entro;
-%         end
-%     end
-%     common_entro(1,col) = -entro;
-%     
-%     seires = mat(:,col);
-%     entro = 0;
-%     s = sum(seires == 1) + sum(seires == 4);
-%     for ind = 1:3:4
-%         p = sum(seires == ind)/s;
-%         if p ~= 0 
-%         entro = p*log2(p) + entro;
-%         end
-%     end
-%     specific_entro(1,col) = -entro;      
-% end
-% 
-% common(isinf(common)) = 0;
-% specific(isinf(specific)) = 0;
-%enmat = zscore(enmat);
-% common = zscore(common);
-% specific = zscore(specific);
-% for row = 1 :len
 
-%     
-%     seires = mat(row,:);
-%     entro = 0;
-%     for ind = min(mat(:)):max(mat(:))
-%         p = sum(seires == ind)/cols;
-%         if p ~= 0 
-%         entro = p*log2(p) + entro;
-%         end
-%     end
+
 %     enmat_spatial(row,1) = -entro;
 % end
 end
